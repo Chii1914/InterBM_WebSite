32631a26e234b33f5867ff6dff95470c9acad69a
Chii1914 <133626251+Chii1914@users.noreply.github.com>
2023-09-26 01:06:35 -0300

    BACK Funcionando

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import {useState} from 'react'
 import axios from "axios"
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Container from '@mui/material/Container';
 
-const Register = () => {
+const Registrar = () => {
+
+
+  
+  const defaultTheme = createTheme();
 
   const [inputs, setInputs] = useState({
     username: "",
     password: "",
+    
   })
 
   const handleChange = e =>{
@@ -16,21 +30,69 @@ const Register = () => {
   const handleSubmit = async e =>{
     e.preventDefault()
     try{
-      let res = await axios.post("/auth/register", inputs)
-      console.log(res)
+      const res = await axios.post("/auth/register", inputs)
+      
     } catch(err){
       console.log(err)
     }
-    
   }
-  console.log(inputs)
+  
+
   return (
-    <div>Register
-    <input required type = "text" placeholder='RUN' name='username' onChange={handleChange} />
-    <input required type = "password" placeholder='password' name='password' onChange={handleChange} />
-    <button onClick={handleSubmit}>Registrate!!</button>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Formulario para registro de personal
+          </Typography>
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required type='text'
+              fullWidth
+              label="RUT"
+              name="username"
+              autoComplete="email"
+              autoFocus
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              required type = 'text'
+              fullWidth
+              name="password"
+              label="Contraseña"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleChange}
+            />
+            
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Registrar persona
+            </Button>
+            
+          </Box>
+        </Box>
+    
+      </Container>
+    </ThemeProvider>
   )
 }
 
-export default Register
+export default Registrar
